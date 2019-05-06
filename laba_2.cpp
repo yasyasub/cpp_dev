dff47021ce9ebc0e6447ab71f19b497e6c71cb2b
Chainii_packetic <yasyasub676@gmail.com>
2019-05-06 13:29:07 +0300

    something not interesting...

--- laba_2.cpp
+++ laba_2.cpp
@@ -3,13 +3,25 @@
 //
 
 #include <iostream>
+#include "stdlib.h"
+#include "map"
 
 template <typename TTT>
 class Demo{
+public:
     TTT data;
 };
 
+struct Point { int x, y; } p1 = {20, 30};
 int main() {
     Demo<double> d;
+    Demo<int> d2;
+    d.data = 2.5;
+    d2.data = 2;
+    Demo<Point> d3;
+    d3.data = p1;
+    Demo<Demo<int>> d4;
+    d4.data.data = 56;
+    std::pair<int, double> ppp;
     return 0;
 }
