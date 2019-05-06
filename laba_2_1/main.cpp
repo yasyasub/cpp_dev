7d5b3efb89c3a3b727dd8252e223d80a89ed4872
Chainii_packetic <yasyasub676@gmail.com>
2019-05-06 13:50:01 +0300

    nothing new

--- laba_2_1/main.cpp
+++ laba_2_1/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <stdlib.h>
+#include <map>
+template <typename TTT>
+class TArr {
+    int n;
+    TTT* mas;
+public:
+    TArr(int a) :n(a)  {
+        if (n > 0) mas = new TTT[n];
+        else {
+            n = 0; mas = NULL;
+        }
+    }
+    TArr() :n(0), mas(NULL) {}
+    virtual ~TArr()  {
+        if (mas) delete[] mas;
+        n = 0;
+    }
+    TArr(const TArr&arg2)  {
+        if (this == &arg2)
+            return;
+        //1
+        this->n = arg2.n;
+        //3
+        mas = new int[n];
+        //4
+        for (int i = 0; i < n; ++i)
+            mas[i] = arg2.mas[i];
+        //5
+        }
+        TArr& operator = (const TArr&arg2)  {
+        if (this == &arg2) return (*this);
+        this->~TArr();
+        //2 звильняемо память
+        this->n = arg2.n;
+        //3
+        mas = new TTT[n];
+        //4
+        for (int i = 0; i < n; ++i)
+            mas[i] = arg2.mas[i];
+        //5
+        return (*this);
+    }
+};
+int main(){
     return 0;
 }
