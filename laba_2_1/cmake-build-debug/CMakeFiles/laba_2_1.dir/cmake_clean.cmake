7d1d99101edc26a9107d7549ced67592c6393cfb
Chainii_packetic <yasyasub676@gmail.com>
2019-05-06 13:33:09 +0300

    laba_2_1

---
+++ laba_2_1/cmake-build-debug/CMakeFiles/laba_2_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba_2_1.dir/main.cpp.o"
+  "laba_2_1.pdb"
+  "laba_2_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba_2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
